79e595772f19d3819816e32353cb947788fad521
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-09-23 12:15:04 +0200

    23.09.2025

--- 005-lekcja-js/04-operatory/operatory.js
+++ 005-lekcja-js/04-operatory/operatory.js
@@ -1,4 +1,47 @@
 let x = 11;
 let y = 5;
 
-// + - * / %
+// + - * / % **
+
+console.log('------ OPERATORY ARYTMETYCNE --------');
+
+let suma = x + y;
+console.log(`Suma: ${suma}`);
+
+let roznica = x - y;
+console.log(`Roznica: ${roznica}`);
+
+let iloczyn = x * y;
+console.log(`Iloczyn: ${iloczyn}`);
+
+let iloraz = x * y;
+console.log(`Iloraz: ${iloraz}`);
+
+let modulo = x % y;
+console.log(`Reszta z dzielenia: ${modulo}`);
+
+let potega = x ** y;
+console.log(`Potegowanie: ${potega}`)
+
+console.log('------ OPERATORY PRZYPISANIA --------');
+// inkrementacja i dekrementacja
+// post oraz pre
+// operatory skrócone += -= *= /= %= **=
+        // na starcie x = 11
+x++;    // 12
+x+=2    // 14
+x-=10;  // 4
+--x;    // 3
+x *= 3; // 9
+x--;    // 9
+x /= 4; // 2
+console.log('x = '+x);
+
+
+
+
+
+
+
+
+
