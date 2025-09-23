7962fcd2d5fe56135a2a40631226d8fad22736d0
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-09-23 14:20:49 +0200

    23.09.2025

--- 006-lekcja-js/cwiczenia.js
+++ 006-lekcja-js/cwiczenia.js
@@ -1,20 +1,17 @@
-/*
-
-    
-
-*/
-
+// 1. Utworz zmienna wynik. Zmienna wynik ma przechowywac informacje czy 15 jest liczba parzysta czy nie. Wynik pokaz w konsoli.
 let wynik = 15;
 console.log(wynik % 2 == 0);
 
+
+// 2. Utworz zmienna liczba i przypisz jej wartosc 8. Za pomoca inkrementacji, zwieksz wartosc zmiennej do 10. Wynik pokaz w konsoli.
 let liczba = 8;
 liczba++;
 liczba++;
 console.log(liczba);
 
+
+// 3. Stworz zmienna x i y. Przypisz do nich liczby 5 i 10. Nastepnie za pomoca operatorow przypisania (zapis skrocony) wypisz w konsoli wynik mnozenia oraz dzielenia x przez y razem z komunikatami wykorzystujac template stringa.
 let x = 5;
 let y = 10;
-console.log(`x * y = ${x *= y}`);
-console.log(`x * y = ${y /= y}`);
-
-
+console.log(`x * y = ${x *= y}`); // template string jest wtedy, gdzy używamy ````````````
+console.log(`x / y = ${x /= y}`);
