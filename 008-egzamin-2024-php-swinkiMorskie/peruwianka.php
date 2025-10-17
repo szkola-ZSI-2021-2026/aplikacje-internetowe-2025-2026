bf51858efa3b5b42f9bd2484d601f7044ade85b4
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-10-17 10:22:02 +0200

    17.10.2025

--- 008-egzamin-2024-php-swinkiMorskie/peruwianka.php
+++ 008-egzamin-2024-php-swinkiMorskie/peruwianka.php
@@ -11,40 +11,18 @@
         <h1>Hodowla świnek morskich - zamów świnkowe maluszki</h1>
     </header>
 
-    <section class="left">
-
-        <nav>
-            <a href="peruwianka.php">Rasa Peruwianka</a>
-            <a href="american.php">Rasa American</a>
-            <a href="crested.php">Rasa Crested</a>
-        </nav>
-
-        <main>
-            <img src="peruwianka.jpg" alt="peruwianka">
-
-            <!-- Skrypt 2 -->
-            <?php
-            $connection = mysqli_connect('localhost','root','','hodowla_mm');
-            
-            
-            ?>
-
-            <hr>
-
-            <h2>Świnki w tym miocie</h2>
-            <!-- Skrypt 3 -->
-            <?php
-            
-            ?>
-        </main>
-        
-    </section>
+    <nav>
+        <a href="peruwianka.php">Rasa Peruwianka</a>
+        <a href="american.php">Rasa American</a>
+        <a href="crested.php">Rasa Crested</a>
+    </nav>
 
     <aside>
         <h3>Poznaj wszystkie rasy świnek morskich</h3>
         <ol>
             <!-- Skrypt 1 -->
             <?php
+            $connection = mysqli_connect('localhost','root','','hodowla_mm');
 
             $query = "SELECT rasy.rasa FROM rasy;";
             
@@ -54,12 +32,48 @@
                 echo "<li> $row[0] </li>";
             }
 
-            mysqli_close($connection);
             
             ?>
         </ol>
     </aside>
 
+    <main>
+        <img src="peruwianka.jpg" alt="peruwianka">
+
+        <!-- Skrypt 2 -->
+        <?php
+          
+        $query = "SELECT DISTINCT swinki.data_ur, swinki.miot, rasy.rasa FROM swinki INNER JOIN rasy ON swinki.rasy_id = rasy.id WHERE rasy.id = 1;";
+            
+        $result = mysqli_query($connection,$query);
+
+        while($row = mysqli_fetch_row($result)) {
+            echo "<h2> Rasa: $row[2] </h2>";
+            echo "<p> Data urodzenia: $row[0] </p>";
+            echo "<p> Oznaczenie miotu: $row[1] </p>";
+        }
+
+        ?>
+
+        <hr>
+
+        <h2>Świnki w tym miocie</h2>
+        <!-- Skrypt 3 -->
+        <?php
+            
+            $query = "SELECT swinki.imie, swinki.cena, swinki.opis FROM swinki WHERE swinki.rasy_id = 1;";
+
+            $result = mysqli_query($connection,$query);
+
+            while($row = mysqli_fetch_row($result)) {
+                echo "<h3> $row[0] - $row[1] </h3>";
+                echo "<p> $row[2] </p>";
+            }
+            mysqli_close($connection);
+
+        ?>
+    </main>
+        
     <footer>
         <p>Stronę wykonał: PESEL</p>
     </footer>
