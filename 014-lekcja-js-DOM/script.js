f1f105138c49dc437c2777f5b5f6d8b5f66107a8
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-10-28 13:05:15 +0100

    28.10.2025

--- 014-lekcja-js-DOM/script.js
+++ 014-lekcja-js-DOM/script.js
@@ -11,13 +11,10 @@ const el2 = document.getElementById('el2');
 console.log(el2);
 
 const tagLi = document.getElementsByTagName('li');
-console.log(tagLi); // HTMLCollection - obiekt tablicopodobny
+console.log(tagLi); // HTMLCollection - obiekt tablicopodobny (żywa kolekcja)
 
 const classElem = document.getElementsByClassName('elem');
 console.log(classElem); // HTMLCollection
-
-
-
 
 // nowsza metoda
 const ol = document.querySelector('ol');
@@ -28,3 +25,64 @@ console.log(el2Id);
 
 const classElem2 = document.querySelectorAll('.elem');
 console.log(classElem2); // NodeList - obeikt tablicopodobny
+
+const tagLi2 = document.querySelectorAll('li');
+console.log(tagLi2); // NodeList
+
+// HTMLCollection vs NodeList
+// HTMLCollection to żywa kolekcja, która przechowuje elementy, które zostały stworzone dynamicznie
+// NodeList przechowuje elementy istniejące "w pliku HTML"
+
+
+// ZADANKO 1
+let header = document.querySelector('h2');
+console.log(header);
+
+let paragrafy = document.querySelectorAll('p');
+console.log(paragrafy);
+
+let blokDiv = document.querySelector('.blok');
+console.log(blokDiv);
+
+let blokDiv_p = blokDiv.querySelector('#akapit'); // zawężenie poszukiwać
+console.log(blokDiv_p);
+
+
+console.log("--- Tworzenie i dodawanie dynamicznych elementów na stronie ---");
+const ulList = document.createElement('ul');
+const listItem = document.createElement('li');
+console.log(document.body);
+console.log(ulList);
+console.log(listItem);
+document.body.appendChild(ulList);
+ulList.appendChild(listItem);
+
+// dodawanie / edytowanie / wyświetlanie treści
+console.log(header.textContent);
+header.textContent = 'test';
+// textContent - zwraca tylko zawartość tekstową elementu, bez żadnych znaczników HTML
+listItem.textContent = 'dodany tekst (textContent)'; // dodawanie
+listItem.textContent = 'zamieniony tekst (textContent)'; // zamiana
+listItem.textContent = '<u>zamieniony tekst (textContent) </u>'; // nie działa
+
+const listItem2 = document.createElement('li');
+ulList.appendChild(listItem2);
+// innerHTML - zwraca całą zawartość HTML danego elementu, nawet ze znacznikami
+listItem2.innerHTML = 'dodany text (innerHTML)'; // dodawanie
+listItem2.innerHTML = 'zamieniony text (innerHTML)'; // zamiana
+listItem2.innerHTML = '<u>2 krotnie zmienoniy tekst text (innerHTML) </u>'; // zamiana
+
+console.log(listItem2.innerHTML);
+
+
+/* 
+    stworz diva w JS, następnie w nim utwórz p, do którego dodasz tekst "dodany paragraf"
+*/
+const div1 = document.createElement('div');
+const div1_paragraf = document.createElement('p');
+document.body.appendChild(div1);
+div1.appendChild(div1_paragraf);
+div1_paragraf.innerHTML = 'dodany paragraf';
+
+
+
