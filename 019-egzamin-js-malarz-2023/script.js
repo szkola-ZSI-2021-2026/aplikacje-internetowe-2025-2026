4d82d13d14ce43b1b486fd5c97e66e944b3b7040
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-11-14 10:21:28 +0100

    14.11.2025

--- 019-egzamin-js-malarz-2023/script.js
+++ 019-egzamin-js-malarz-2023/script.js
@@ -1,21 +1,20 @@
 const buttonPolicz = document.querySelector('#policz');
 const m2 = document.querySelector('#m2');
+const divObliczonyWynik = document.querySelector('#obliczonyWynik');
 
 function policzPowierzchnie() {
-    // 1 piuszka = 1 l farby
+    // 1 puszka = 1 l farby
     // 1 l farby = 4m2
     console.log('wejscie do funkcji');
     console.log(m2.value);
 
-    
+    let powierzchnia = m2.value;
 
+    let liczbaPotrzbnychPuszek = Math.ceil(powierzchnia / 4);
 
+    console.log(liczbaPotrzbnychPuszek);
+
+    divObliczonyWynik.innerHTML = `<p>Liczba potrzebnych puszek: ${liczbaPotrzbnychPuszek} </p>`;
 }
 
-
-
-
-
 buttonPolicz.addEventListener('click', policzPowierzchnie);
-
-//filtry -> romycie pikseli
