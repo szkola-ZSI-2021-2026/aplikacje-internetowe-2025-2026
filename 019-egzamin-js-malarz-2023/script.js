2d976b723e22fcba7ff8a66efd6a1bd394b400cb
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-11-14 09:36:26 +0100

    14.11.2025

---
+++ 019-egzamin-js-malarz-2023/script.js
@@ -0,0 +1,21 @@
+const buttonPolicz = document.querySelector('#policz');
+const m2 = document.querySelector('#m2');
+
+function policzPowierzchnie() {
+    // 1 piuszka = 1 l farby
+    // 1 l farby = 4m2
+    console.log('wejscie do funkcji');
+    console.log(m2.value);
+
+    
+
+
+}
+
+
+
+
+
+buttonPolicz.addEventListener('click', policzPowierzchnie);
+
+//filtry -> romycie pikseli
