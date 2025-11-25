95a69f7dc3cf1ff944b1503c775bb5ad00320c22
Michal12351 <139280501+Michal12351@users.noreply.github.com>
2025-11-25 14:37:01 +0100

    25.11.2025

--- 024-egzamin-js-styleGikraltar-2022/script.js
+++ 024-egzamin-js-styleGikraltar-2022/script.js
@@ -2,31 +2,51 @@ const indigoButton = document.querySelector('#indigo');
 const steelBlueButton = document.querySelector('#steelBlue');
 const oliveButton = document.querySelector('#olive');
 const main = document.querySelector('main');
-const fontColorListOption = document.querySelectorAll('.options');
-const fontColorList = document.querySelectorAll('.fontColorList');
-const fontSizeInput = document.querySelectorAll('#fontSize');
+const mainImg = document.querySelector('#mainImg');
+const czyRamkaCheckBox = document.querySelector('#czyRamka');
+const fontColorList = document.querySelectorAll('#fontColorList');
+const fontSizeInput = document.querySelector('#fontSize');
+const radioInputs = document.querySelectorAll('.radioInput');
+const unorderedList = document.querySelector('ul');
 
 function kolorTla(kolor) {
     main.style.backgroundColor = kolor;
 }
 
-function kolorCzcionki(option) {
-    console.log(option);
-    console.log(option.value);
+function kolorCzcionki(kolor) {
+    main.style.color = kolor;
 }
 
-console.log(fontColorList);
-
-fontColorListOption.forEach( (option) => {
-    option.addEventListener('click', () => kolorCzcionki(option.value));
-});
-
 function rozmiarCzcionki() {
-    console.log('rozmiar');
     main.style.fontSize = fontSizeInput.value;
+}
+
+let czyRamka = 1;  
+function zmianaStanuRamki() {
+    switch (czyRamka) {
+        case 0:
+            mainImg.style.border = '1px solid white';
+            czyRamka = 1;
+            break;
+        case 1:
+            mainImg.style.border = 'none';
+            czyRamka = 0;
+            break;
+    }
+}
+
+function zmianaPunktowListy(radio) {
+    unorderedList.style.listStyleType = `${radio.value}`;
 }
 
 indigoButton.addEventListener('click', () => kolorTla('indigo'));
 steelBlueButton.addEventListener('click', () => kolorTla('steelBlue'));
 oliveButton.addEventListener('click', () => kolorTla('olive'));
 fontSizeInput.addEventListener('blur', () => rozmiarCzcionki());
+fontColorList.forEach( (option) => {
+    option.addEventListener('change', () => kolorCzcionki(option.value));
+});
+czyRamkaCheckBox.addEventListener('click', zmianaStanuRamki);
+radioInputs.forEach( (radio) => {
+    radio.addEventListener('click', () => zmianaPunktowListy(radio));
+});
